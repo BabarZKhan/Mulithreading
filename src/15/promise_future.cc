fb344af58ad4f450d32ae4045edc02115a37134f
ubuntu <bzamankhan@gmail.com>
2021-04-23 12:52:53 +0200

    15 and 16

---
+++ src/15/promise_future.cc
@@ -0,0 +1,55 @@
+// TOPIC: std::future and std::promise in threading
+
+//NOTES
+// 1. std::promise
+//    a. Used to set values or exceptions
+// 2. std::future
+//    a. Used to get values from promise 
+//    b. Ask promise if the value is available
+//    c. Wait for the promise
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <future>
+
+using namespace std;
+using namespace std::chrono;
+typedef long int ull;
+
+void findOdd(std::promise<ull>&& OddSumPromise, ull start, ull end) {
+	ull OddSum = 0;
+	for (ull i = start; i <= end; ++i) {
+		if (i & 1) {
+			OddSum += i;
+		}
+	}
+	OddSumPromise.set_value(OddSum);
+	
+}
+
+int main() {
+	
+	ull start = 0;
+	ull end = 1900000000;
+	
+	// there is a state sharing between st::promise and std::future
+	
+	std::promise<ull> OddSum; //promise object 
+	std::future<ull> OddFuture = OddSum.get_future(); //future object 
+	
+	cout << "Thread Created" << endl;
+	std::thread t1(findOdd, std::move(OddSum), start, end);	//function pointer
+	
+	cout << "Waiting for result" << endl;
+	
+	cout << "OddSum :" << OddFuture.get() << endl;
+	
+	cout << " Completed" << endl;
+	
+	t1.join();
+	
+	return 0;
+	
+}			
---
+++ src/16/std_async.cc
@@ -0,0 +1,51 @@
+
+// TOPIC : async
+
+// NOTES:
+// 1. It runs a function asynchronously (potentially in a new thread) and returns std::future
+//    that will hold the result.
+
+// 2. There are three launch policies for creating tasks:
+//    a. std::launch ::async
+//    b. std::launch::defered      
+//    c. std::launch::async | std::launch::async 
+
+
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <future>
+#include <algorithm>
+
+using namespace std;
+using namespace std::chrono;
+typedef long int ull;
+
+
+ull findOdd(ull start, ull end) {
+	ull OddSum = 0;
+	cout << "Thread ID of function findOdd is: " <<std::this_thread::get_id() << endl;
+	for (ull i= start; i<= end; i++) {
+		if (i & 1) {
+			OddSum += i;
+		}
+	}
+	return OddSum;
+}
+
+int main () {
+	
+	ull start = 0, end = 1900000000;
+	
+	cout << "Thread ID of fmain () function is: " <<std::this_thread::get_id() << endl;
+	cout << "Thread created if policy is std::launch::async!" << endl;		
+    std::future<ull> OddSum = std::async(std::launch::deferred, findOdd, start, end);
+    
+    cout <<"Waiting for Result" << endl;
+    cout << "OddSum : " << OddSum.get() << endl;
+    
+    cout << "Completed" << endl;
+    return 0;
+}
